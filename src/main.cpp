bf0e82acbf1ebc5665156a1e0629b26c04eba8ec
Aleks Rutins <aleks@rutins.com>
2023-05-26 21:12:20 -0400

    static_assert the right thing

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 #include <boost/spirit/version.hpp>
 
-static_assert(SPIRIT_VERSION >= 2059, "Boost::Spirit is too old, please update");
+static_assert(SPIRIT_VERSION >= 0x2059, "Boost::Spirit is too old, please update");
 
 int main() {
 	std::cout << "Hello World\n";
