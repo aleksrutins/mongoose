a8604d169c51a10949ecc38961015dbd6ea8ee11
Aleks Rutins <aleks@rutins.com>
2023-05-26 21:11:14 -0400

    Add Boost::Spirit

--- src/Mongoose/Parser.hpp
+++ src/Mongoose/Parser.hpp
@@ -1,5 +1,5 @@
 namespace Mongoose {
     class Parser {
 
-    }
+    };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include <boost/spirit/version.hpp>
+
+static_assert(SPIRIT_VERSION >= 2059, "Boost::Spirit is too old, please update");
+
 int main() {
 	std::cout << "Hello World\n";
 	return 0;
